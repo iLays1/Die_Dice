2b9dd15356a5f9a1b4ed36a6ed08dcfd09c8ffb2
iLays <iliaslays@gmail.com>
2022-06-06 16:58:48 -0600

    Test Failed, Keeping it simple

--- Assets/_project/_Scripts/Combat/Managers/CombatManager.cs
+++ Assets/_project/_Scripts/Combat/Managers/CombatManager.cs
@@ -68,18 +68,18 @@ public class CombatManager : Singleton<CombatManager>
         yield return new WaitForSeconds(0.2f);
 
         var opos = playerUnit.transform.position;
-        if (diceManager.totalAttacksRolled > 0)
+        for (int i = 0; i < diceManager.totalAttacksRolled; i++)
         {
             //Player Attack code
             playerUnit.transform.DOComplete();
             Sequence s = DOTween.Sequence();
-            s.Append(playerUnit.transform.DORotate(new Vector3(75f, 90, -90), 0.4f));
-            s.Append(playerUnit.transform.DORotate(new Vector3(110, 90, -90), 0.15f).SetEase(Ease.OutBack));
-            s.Append(playerUnit.transform.DORotate(new Vector3(90, 0, -180), 0.15f));
-            
-            yield return new WaitForSeconds(0.4f);
-            enemyUnit.TakeDamage(GameDataSystem.Instance.attackPower * diceManager.totalAttacksRolled);
-            yield return new WaitForSeconds(0.2f);
+            s.Append(playerUnit.transform.DORotate(new Vector3(75f, 90, -90), 0.3f));
+            s.Append(playerUnit.transform.DORotate(new Vector3(110, 90, -90), 0.1f).SetEase(Ease.OutBack));
+            s.Append(playerUnit.transform.DORotate(new Vector3(90, 0, -180), 0.3f));
+
+            yield return new WaitForSeconds(0.3f);
+            enemyUnit.TakeDamage(GameDataSystem.Instance.attackPower);
+            yield return new WaitForSeconds(0.1f);
         }
         playerUnit.transform.DOMove(opos, 0.3f);
 
